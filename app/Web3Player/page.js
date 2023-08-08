4511d6cc41220b0c1a0ed8ccc6971392c47066e7
mubarakone <52806204+mubarakone@users.noreply.github.com>
2023-08-07 19:23:20 -0700

    add video

---
+++ app/Web3Player/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Web3Player() {
+    const video = "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4"
+
+  return (
+    <video 
+        src={video}
+    />
+  )
+}
